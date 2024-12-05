2c46454bb4c00066869bbcf53b8d6655fea7178f
Bruno Tomé <btomeduarte@gmail.com>
2024-12-05 11:34:13 -0300

    Feat: Moldando rotas e mixins

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -8,6 +8,15 @@ class PessoaController extends Controller {
         super(pessoaServices)
     }
 
+    async pegaMatricula(req,res) {
+        const {estudanteId } = req.params;
+        try {
+            const listamatriculas = await pessoaServices.pegaMatriculaPorEstudante(Number(estudanteId));
+            return res.sttaus(200).json(listaMatricula);
+        } catch (error) {
+            
+        }
+    }
 }  
 
 module.exports = PessoaController;
--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -4,6 +4,12 @@ class PessoaServices extends Services {
     constructor() {
         super('Pessoa')
     }
+
+    async pegaMatriculaPorEstudante(id) {
+        const estudante = await super.pegaUmRegistroPorId(id);
+        const listaMatricula = await estudante.getAulasMatriculadas();
+        return listaMatricula;
+    }
 }
 
 module.exports = PessoaServices;
